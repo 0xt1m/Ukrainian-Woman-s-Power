689170008e91ece304d9409503a85357b1eaee41
0prisnyk <denysoprisnyk@gmail.com>
2022-04-30 16:00:47 +0300

    first commit

---
+++ Ukrainian_Power_Remastered.py
@@ -0,0 +1,229 @@
+import pygame
+import os
+from pygame.locals import *
+
+pygame.init()
+
+width = 600
+height = 400
+
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Ukrainian Woman's Power")
+
+clock = pygame.time.Clock()
+FPS = 60
+
+GRAVITY = 0.75
+
+moving_left = False
+moving_right = False
+shoot = False
+
+
+bullet_img = pygame.image.load("bullet.png").convert_alpha()
+
+
+BG = (150, 150, 150)
+RED = (255, 0, 0)
+
+
+def draw_bg():
+    screen.fill(BG)
+    pygame.draw.line(screen, RED, (0, 300), (width, 300))
+
+
+class Person(pygame.sprite.Sprite):
+    def __init__(self, char_type, x, y, scale, speed, ammo):
+        pygame.sprite.Sprite.__init__(self)
+        self.alive = True
+        self.char_type = char_type
+        self.speed = speed
+        self.ammo = ammo
+        self.start_ammo = ammo
+        self.shoot_cooldown = 0
+        self.health = 100
+        self.max_health = self.health
+        self.direction = 1
+        self.vel_y = 0
+        self.jump = False
+        self.in_air = True
+        self.flip = False
+        self.animation_list = []
+        self.index = 0
+        self.action = 0
+        self.update_time = pygame.time.get_ticks()
+
+        animation_types = ['Idle', 'Run', 'Jump', 'Death']
+
+        for animation in animation_types:
+            temp_list = []
+            number_of_frames = len(os.listdir(f'{char_type}/{animation}'))
+            for i in range(number_of_frames):
+                img = pygame.image.load(f'{char_type}/{animation}/{i}.jpg').convert_alpha()
+                img = pygame.transform.scale(img, (img.get_width() / scale, img.get_height() / scale))
+                temp_list.append(img)
+            self.animation_list.append(temp_list)
+
+        self.image = self.animation_list[self.action][self.index]
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+
+    def update(self):
+        self.update_animation()
+        self.check_alive()
+        if self.shoot_cooldown > 0:
+            self.shoot_cooldown -= 1
+
+    def move(self, moving_left, moving_right):
+        dx = 0
+        dy = 0
+
+        if moving_left:
+            dx = -self.speed
+            self.flip = True
+            self.direction = -1
+        if moving_right:
+            dx = self.speed
+            self.flip = False
+            self.direction = 1
+
+        if self.jump is True and self.in_air is False:
+            self.vel_y = -11
+            self.jump = False
+            self.in_air = True
+
+        self.vel_y += GRAVITY
+        if self.vel_y > 10:
+            self.vel_y
+        dy += self.vel_y
+
+        if self.rect.bottom + dy > 300:  # red line
+            dy = 300 - self.rect.bottom
+            self.in_air = False
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+    def shoot(self):
+        if self.shoot_cooldown == 0 and self.ammo > 0:
+            self.shoot_cooldown = 20
+            bullet = Bullet(self.rect.centerx + (self.rect.size[0] * 0.6 * self.direction), self.rect.centery, self.direction)
+            bullet_group.add(bullet)
+            self.ammo -= 1
+
+    def update_animation(self):
+        ANIMATION_COOLDOWN = 100
+        self.image = self.animation_list[self.action][self.index]
+        if pygame.time.get_ticks() - self.update_time > ANIMATION_COOLDOWN:
+            self.update_time = pygame.time.get_ticks()
+            self.index += 1
+        if self.index >= len(self.animation_list[self.action]):
+            if self.action == 3:
+                self.index = len(self.animation_list[self.action]) - 1
+            else:
+                self.index = 0
+
+    def update_action(self, new_action):
+        if new_action != self.action:
+            self.action = new_action
+            self.index = 0
+            self.update_time = pygame.time.get_ticks()
+
+    def check_alive(self):
+        if self.health <= 0:
+            self.health = 0
+            self.speed = 0
+            self.alive = False
+            self.update_action(3)
+
+    def draw(self):
+        screen.blit(pygame.transform.flip(self.image, self.flip, False), self.rect)
+
+
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y, direction):
+        pygame.sprite.Sprite.__init__(self)
+        self.speed = 10
+        self.image = bullet_img
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+        self.direction = direction
+
+    def update(self):
+        self.rect.x += (self.direction * self.speed)
+        if self.rect.right < 0 or self.rect.left > width:
+            self.kill()
+
+        if pygame.sprite.spritecollide(player, bullet_group, False):
+            if player.alive:
+                player.health -= 5
+                self.kill()
+        if pygame.sprite.spritecollide(enemy, bullet_group, False):
+            if enemy.alive:
+                enemy.health -= 25
+                self.kill()
+
+
+
+
+
+bullet_group = pygame.sprite.Group()
+
+player = Person("Granny", 300, 200, 5, 3, 20)
+enemy = Person("Granny", 500, 240, 5, 3, 20)
+
+
+x = 300
+y = 200
+scale = 5
+
+run = True
+while run:
+
+    clock.tick(FPS)
+    draw_bg()
+    player.update()
+    player.draw()
+    enemy.update()
+    enemy.draw()
+
+    bullet_group.update()
+    bullet_group.draw(screen)
+
+    if player.alive:
+        if shoot:
+            player.shoot()
+        if player.in_air:
+            player.update_action(2)
+        elif moving_left or moving_right:
+            player.update_action(1)
+        else:
+            player.update_action(0)
+        player.move(moving_left, moving_right)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_SPACE:
+                shoot = True
+            if event.key == pygame.K_w and player.alive:
+                player.jump = True
+            if event.key == pygame.K_ESCAPE:
+                run = False
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_SPACE:
+                shoot = False
+
+    pygame.display.update()
+
+pygame.quit()
